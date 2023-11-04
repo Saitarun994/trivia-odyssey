fc0373e20d4215b34f5180986cc34d925b3f6347
neanderthalensis <atyrlik@terpmail.umd.edu>
2023-11-03 21:39:14 -0400

    test

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-
-const express = require('express')
-const app = express()
-const path = require('path')
-
+const app = require('./app')
 
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
-// app.all('/', (req, res) => {
-//     console.log("Just got a request!")
-//     res.send('Yo!')
-// })
-// app.listen(process.env.PORT || 3000)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
