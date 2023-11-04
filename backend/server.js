0a1ca566321fc60cd6be036b36c91117565d991c
unknown <taruns994@gmail.com>
2023-11-04 17:20:11 -0400

    frontend cards

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const CyclicDb = require("@cyclic.sh/dynamodb")
 
 
 const port = process.env.PORT || 3000; 
+
+
+async () => {
+  const db = CyclicDb("plum-wandering-vultureCyclicDB")
+  console.log("blablabl")
+}
+
 
 
 app.use(express.static(path.join(__dirname, 'build')))
@@ -15,21 +22,31 @@ app.get("/", (req, res) => {
 
 
 app.get("/api/rochester/:lat&:lon", (req, res) =>{
-	res.send({ilnk: "", name: "Highland park", desc: "Its a park", link: "maps.google.com"})
+	res.send([{ilnk: "/assets/highlandpark.png", name: "Highland park", desc: "Its a asdfasdf asdfasdf asdfasdf adsfasdf asdf park", link: "maps.google.com"},
+  {ilnk: "/assets/highlandpark.png", name: "second park", desc: "Its a asdfasdf  asdfasdf adsfasdf asdf park", link: "maps.google.com"},
+  {ilnk: "/assets/highlandpark.png", name: "third park", desc: "Its a asdfasdf asdfasdf atrewt6y adsfasdf asdf park", link: "maps.google.com"}
+])
 })
 
+// app.post('api/rochester/:col&:key', async (req, res) => {
+//   console.log(req.body)
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} insert key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).set(key, req.body)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
 
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
+// app.get("/api/rochester/:col&:key", async (req, res) => {
+//   console.log(req.body)
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).get(key)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
 
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
 
-
-console.log("this changed")
 app.listen(port, () => console.log(`Listening on port ${port}`))
