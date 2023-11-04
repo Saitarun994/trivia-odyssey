7118710efe1e5a7007ff209aef46a0597a2310db
neanderthalensis <atyrlik@terpmail.umd.edu>
2023-11-04 12:02:53 -0400

    try again agina

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const path = require('path');
 const port = process.env.PORT || 3000; 
 
 
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
