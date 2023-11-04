d130b40fa5cfc689d8cb456d0b9dce283374957d
neanderthalensis <atyrlik@terpmail.umd.edu>
2023-11-03 21:32:55 -0400

    test

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.use(express.static('/test.html'))
 
 
 
 
 
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.send(express.static('/test.html'))
 })
 app.listen(process.env.PORT || 3000)
