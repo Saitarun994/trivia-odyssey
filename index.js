6035c0b020d155ebdcb02d812518832bf29b7003
neanderthalensis <atyrlik@terpmail.umd.edu>
2023-11-03 21:31:37 -0400

    test

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.static('/test.html'))
 
 
 
-// app.all('/', (req, res) => {
-//     console.log("Just got a request!")
-//     res.send('Yo!')
-// })
-// app.listen(process.env.PORT || 3000)
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
