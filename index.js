9df87c58c59eb12b7cc515555869388db298eee8
neanderthalensis <atyrlik@terpmail.umd.edu>
2023-11-03 21:55:29 -0400

    static

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const app = require('./app')
+const express = require('express');
+const app = express();
+const path = require('path')
+
 
 const port = process.env.PORT || 3000
+
+app.use("/", express.static(path.join(__dirname, "../public")));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/front-end/webpage/dist/index.html'));
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
