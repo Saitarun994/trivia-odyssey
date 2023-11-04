63423421b8bdec5889c9d899d6aa9685976601c6
neanderthalensis <atyrlik@terpmail.umd.edu>
2023-11-03 21:10:01 -0400

    initial back end third try

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+
+
+app.use(express.static('front-end/webpage'))
+
+
+
+
+
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+// app.listen(process.env.PORT || 3000)
